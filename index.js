a5e71e7859100b607fdf3159e9209bb14676ee15
Dennis Ding <xuchu.ding@gmail.com>
2023-12-12 14:49:36 -0800

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express'
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const https = require("https"),
+    fs = require("fs");
+
+const options = {
+    key: fs.readFileSync("./private-key.pem"),
+    cert: fs.readFileSync("./public-cert.pem")
+};
+
 const app = express()
 
 app.listen(5001, () => console.log('Api running on port 5001'))
 
 app.get('/', (req, res) => res.json('My API is running'))
+
+https.createServer(options, app).listen(5051);
